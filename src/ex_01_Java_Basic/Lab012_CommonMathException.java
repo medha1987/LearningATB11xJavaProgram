08156a55b64141f53ffffabf0c371a4c132669c1
Mandar <m19.kulkarni@gmail.com>
2025-03-11 14:29:44 +0530

    test commit

---
+++ src/ex_01_Java_Basic/Lab012_CommonMathException.java
@@ -0,0 +1,8 @@
+package ex_01_Java_Basic;
+
+public class Lab012_CommonMathException {
+    public static void main(String[] args) {
+
+        System.out.println(10/0); // "main" java.lang.ArithmeticException: / by zero
+    }
+}
